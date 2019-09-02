c27cc47b958af20dbccf239eafcfcba77b4bf8cd
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-09-02 15:26:56 +0300

    Add .editorconfig

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,20 @@ use std::fs::DirEntry;
 use finder::Finder;
 
 fn is_font_file(e: &DirEntry) -> bool {
-    if let Some(s) = e.path().file_name() {
-        let name = String::from(s.to_str().unwrap());
+  if let Some(s) = e.path().file_name() {
+    let name = String::from(s.to_str().unwrap());
 
-        if (name.ends_with(".ttf") || name.ends_with(".ttc")) {
-            return true;
-        }
+    if (name.ends_with(".ttf") || name.ends_with(".ttc")) {
+      return true;
     }
+  }
 
-    false
+  false
 }
 
 fn main() {
-    let finders = Finder::new("/usr/share/fonts/opentype:/usr/share/fonts/truetype/noto");
-    for i in finders.filter(&is_font_file).into_iter() {
-        println!("{}", i.path().to_str().expect("FAIL!!!!"));
-    }
+  let finders = Finder::new("/usr/share/fonts/opentype:/usr/share/fonts/truetype/noto");
+  for i in finders.filter(&is_font_file).into_iter() {
+    println!("{}", i.path().to_str().expect("FAIL!!!!"));
+  }
 }
