a1d0a63b479980f4b64935c669cdd9c5ef4962a8
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-09-15 20:22:01 +0300

    Added new tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -22,8 +22,6 @@ fn empty() {
 
   //Assert
   assert_eq!(collection.len(), 0);
-
-  println!("Test empty directory");
   assert_eq!(finderResult.len(), 0);
 }
 
@@ -41,7 +39,90 @@ fn one_file() {
 
   //Assert
   assert_eq!(collection.len(), 1);
-
-  println!("Test one file in directory");
   assert_eq!(finderResult.len(), 1);
+}
+
+#[test]
+fn some_files() {
+  //Arrange
+  let tmp = TempDir::new("fonthelper_tests").unwrap();
+  let collection = DataSet::new(&tmp.path()).some_files();
+
+  //Act
+  let finderResult: Vec<String> = Finder::new(&tmp.path())
+    .into_iter()
+    .map(|e| e.path().to_str().unwrap().to_string())
+    .collect();
+
+  //Assert
+  assert_eq!(collection.len(), 8);
+  assert_eq!(finderResult.len(), 8);
+}
+
+#[test]
+fn one_depth() {
+  //Arrange
+  let tmp = TempDir::new("fonthelper_tests").unwrap();
+  let collection = DataSet::new(&tmp.path()).one_depth();
+
+  //Act
+  let finderResult: Vec<String> = Finder::new(&tmp.path())
+    .into_iter()
+    .map(|e| e.path().to_str().unwrap().to_string())
+    .collect();
+
+  //Assert
+  assert_eq!(collection.len(), 8);
+  assert_eq!(finderResult.len(), 8);
+}
+
+#[test]
+fn two_depth() {
+  //Arrange
+  let tmp = TempDir::new("fonthelper_tests").unwrap();
+  let collection = DataSet::new(&tmp.path()).two_depth();
+
+  //Act
+  let finderResult: Vec<String> = Finder::new(&tmp.path())
+    .into_iter()
+    .map(|e| e.path().to_str().unwrap().to_string())
+    .collect();
+
+  //Assert
+  assert_eq!(collection.len(), 10);
+  assert_eq!(finderResult.len(), 10);
+}
+
+#[test]
+fn five_depth() {
+  //Arrange
+  let tmp = TempDir::new("fonthelper_tests").unwrap();
+  let collection = DataSet::new(&tmp.path()).five_depth();
+
+  //Act
+  let finderResult: Vec<String> = Finder::new(&tmp.path())
+    .into_iter()
+    .map(|e| e.path().to_str().unwrap().to_string())
+    .collect();
+
+  //Assert
+  assert_eq!(collection.len(), 14);
+  assert_eq!(finderResult.len(), 14);
+}
+
+#[test]
+fn test_dataset_1() {
+  //Arrange
+  let tmp = TempDir::new("fonthelper_tests").unwrap();
+  let collection = DataSet::new(&tmp.path()).dataset_1();
+
+  //Act
+  let finderResult: Vec<String> = Finder::new(&tmp.path())
+    .into_iter()
+    .map(|e| e.path().to_str().unwrap().to_string())
+    .collect();
+
+  //Assert
+  assert_eq!(collection.len(), 800);
+  assert_eq!(finderResult.len(), 800);
 }
--- src/tests/utils.rs
+++ src/tests/utils.rs
@@ -5,6 +5,8 @@ use std::{
   fs,
   path::{Path, PathBuf},
 };
+#[cfg(test)]
+use tempdir::TempDir;
 
 pub struct DataSet {
   root: PathBuf,
@@ -20,12 +22,11 @@ impl DataSet {
   pub fn empty_dir(&self) -> Vec<String> {
     vec![]
   }
+
   pub fn one_file(&self) -> Vec<String> {
-    let files: Vec<String> = vec![String::from(format!(
-      "{}/{}",
-      self.root.to_str().unwrap(),
-      "image.jpg"
-    ))];
+    let files: Vec<String> = vec![
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "image.jpg"))
+    ];
 
     for file in &files {
       fs::write(file, "").unwrap();
@@ -33,5 +34,135 @@ impl DataSet {
 
     files
   }
-  pub fn some_files(&self, count: i32) {}
+
+  pub fn some_files(&self) -> Vec<String> {
+    let files: Vec<String> = vec![
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "image.jpg")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial Italic.ttf")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial bold.ttf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Arial.ttc")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Cuber.otf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Im.otf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "logo.jpeg")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "lol.png")),
+    ];
+
+    for file in &files {
+      fs::write(file, "").unwrap();
+    }
+
+    files
+  }
+
+  pub fn one_depth(&self) -> Vec<String> {
+    let paths: Vec<String> = vec![
+      format!("{}/{}", self.root.to_str().unwrap(), "images")
+    ];
+    let files: Vec<String> = vec![
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/image.jpg")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial Italic.ttf")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial bold.ttf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Arial.ttc")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Cuber.otf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Im.otf")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/logo.jpeg")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/lol.png")),
+    ];
+
+    for path in &paths {
+      fs::create_dir_all(path).unwrap();
+    }
+
+    for file in &files {
+      fs::write(file, "").unwrap();
+    }
+
+    files
+  }
+
+  pub fn two_depth(&self) -> Vec<String> {
+    let paths: Vec<String> = vec![
+      format!("{}/{}", self.root.to_str().unwrap(), "images/meme"),
+    ];
+    let files: Vec<String> = vec![
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/image.jpg")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial Italic.ttf")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial bold.ttf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Arial.ttc")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Cuber.otf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Im.otf")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/logo.jpeg")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/lol.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/bird.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/ThisIsFine.png")),
+    ];
+
+    for path in &paths {
+      fs::create_dir_all(path).unwrap();
+    }
+
+    for file in &files {
+      fs::write(file, "").unwrap();
+    }
+
+    files
+  }
+
+  pub fn five_depth(&self) -> Vec<String> {
+    let paths: Vec<String> = vec![
+      format!("{}/{}", self.root.to_str().unwrap(), "images/meme/3/4/5"),
+    ];
+    let files: Vec<String> = vec![
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/image.jpg")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial Italic.ttf")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "Arial bold.ttf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Arial.ttc")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Cuber.otf")),
+      String::from(format!("{}/{}", self.root.to_str().unwrap(), "Im.otf")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/logo.jpeg")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/3/lol.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/3/4/bird.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/3/4/bird1.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/3/4/bird2.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/3/4/5/ThisIsFine.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/3/4/5/meme1.png")),
+      String::from(format!("{}/{}",self.root.to_str().unwrap(), "images/meme/3/4/5/meme2.png")),
+    ];
+
+    for path in &paths {
+      fs::create_dir_all(path).unwrap();
+    }
+
+    for file in &files {
+      fs::write(file, "").unwrap();
+    }
+
+    files
+  }
+
+  pub fn dataset_1(&self) -> Vec<String> {
+    let mut files: Vec<String> = vec![];
+    let paths: Vec<String> = vec![
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts"),
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts/truetype"),
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts/opentype"),
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts/X11"),
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts/cmap"),
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts/font-awesome"),
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts/type1"),
+      format!("{}/{}", self.root.to_str().unwrap(), "fonts/users"),
+    ];
+
+    for path in &paths {
+      fs::create_dir_all(path).unwrap();
+
+      for index in 0..100 {
+        fs::write(format!("{}/file_{}.ttf", &path, &index), "").unwrap();
+
+        files.push(format!("{}/file_{}.ttf", &path, &index));
+      }
+    }
+
+    files
+  }
 }
